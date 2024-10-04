a7207663ebfc015387f531974e97a8465b1d5fea
Jeewapadma <udithjeewapadma117@gmail.com>
2024-10-04 16:00:04 +0530

    create and style topDoctors component and setup navigate

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpecialtyMenu from '../components/SpecialtyMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <SpecialtyMenu/>
+      <TopDoctors/>
     </div>
   )
 }
