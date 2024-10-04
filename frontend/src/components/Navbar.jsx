a7c09a0e44b774a6b1fa6efec2bb7a61e2194358
Jeewapadma <udithjeewapadma117@gmail.com>
2024-10-04 08:17:25 +0530

    create navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {assets} from '../assets/assets';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+const Navbar = () => {
+    const navigate = useNavigate();
+  return (
+    <div className=' flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-500'>
+        <img className=' w-44 cursor-pointer' src={assets.logo} alt="" />
+        <ul className=' hidden md:flex items-start gap-5 font-medium '>
+            <NavLink to='/'>
+                <li className=' py-1'>Home</li>
+                <hr className=' border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+            </NavLink>
+
+            <NavLink to='/doctors'>
+                <li  className=' py-1'>ALL Doctors</li>
+                <hr className=' border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+            </NavLink>
+
+            <NavLink to='/about'>
+                <li className=' py-1'>About</li>
+                <hr className=' border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+            </NavLink>          
+
+            <NavLink to='/contact'>
+                <li className=' py-1'>Contact</li>
+                <hr className=' border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+            </NavLink>
+        </ul>
+        <div className=' flex items-center gap-4'>
+            <button onClick={()=>navigate('/login')} className=' bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
