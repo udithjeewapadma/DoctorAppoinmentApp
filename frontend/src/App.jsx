79e9700f9e5de3feafc2072fbeecd79bfe5b73d6
Jeewapadma <udithjeewapadma117@gmail.com>
2024-10-04 07:32:20 +0530

    define routes and create folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
 
 const App = () => {
   return (
-    <div className='text-green-500'>App</div>
+    <div className=' mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:specialty' element={<Doctors />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/appointment/:docId' element={<Appointment/>} />
+      </Routes>
+    </div>
   )
 }
 
